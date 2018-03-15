0b9199d607503fa2da99b2ccae186c28d37f519a
maksagit <maksibon@gmail.com>
2018-03-15 23:43:59 +0300

    longest-consecutive-sequence test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
+  
+	if (array.length == 0) return 0;
+	if (array.length == 1) return 1;
+    
+    // быстрая сортировка массива
+    array.sort((a, b) => a - b);
+	
+    var max = 0; // максимальная последовательность
+    var count = 1; // счетчик
+	
+    for (let i = 0; i <= array.length; i++) {
+        if (array[i] == array[i + 1]) continue;
+        if (array[i] + 1 == array[i + 1]) {
+            count++;
+        } else {
+            count = 1;
+        }
+        max = Math.max(count, max);
+    }
+    return max;
 }
